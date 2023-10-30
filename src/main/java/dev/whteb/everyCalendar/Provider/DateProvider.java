ee88295f6f45ed4de016394fea5b6ed1faa788e4
조민수 <whiteblock0715@gmail.com>
2023-10-30 14:41:18 +0900

    fix: Calendar 타임존 설정 코드 추가
    - DateProvider: 타임존을 Asia/Seoul로 지정
    
    issue: #14

--- src/main/java/dev/whteb/everyCalendar/Provider/DateProvider.java
+++ src/main/java/dev/whteb/everyCalendar/Provider/DateProvider.java
@@ -5,12 +5,14 @@ import org.springframework.stereotype.Component;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 @Component
 public class DateProvider {
     public Date findNearestWeekDay(int weekDay, Date startDate) {
         Calendar cal = Calendar.getInstance(Locale.KOREA);
         cal.setTime(startDate);
+        cal.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
 
         cal.set(Calendar.HOUR_OF_DAY, 0);
         cal.clear(Calendar.MINUTE);
