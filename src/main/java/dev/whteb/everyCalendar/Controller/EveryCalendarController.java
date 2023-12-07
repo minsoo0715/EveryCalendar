f059d6c08566a9f8340e5e084e2f8c35fe928e6c
조민수 <whiteblock0715@gmail.com>
2023-12-08 00:39:37 +0900

    ref: ContentDisposition 객체로 관리하도록 변경
    - EveryCalendarController: success()에서 Content Disposition 객체를 생성해 사용하도록 변경
    
    issue: #31

--- src/main/java/dev/whteb/everyCalendar/Controller/EveryCalendarController.java
+++ src/main/java/dev/whteb/everyCalendar/Controller/EveryCalendarController.java
@@ -4,12 +4,14 @@ import dev.whteb.everyCalendar.DTO.GetIcsDTO;
 import dev.whteb.everyCalendar.Service.EveryCalendarService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,9 +33,8 @@ public class EveryCalendarController {
         Pattern pattern = Pattern.compile("(?<=(https://everytime.kr/@))[A-Za-z0-9]{1,}");
         Matcher matcher = pattern.matcher(calendarUrl);
 
-        if(!matcher.find()) {
+        if(!matcher.find())
             throw new Exception("시간표 URL이 올바르지 않습니다.");
-        }
 
         String identifier = matcher.group();
 
@@ -45,8 +46,11 @@ public class EveryCalendarController {
 
     @GetMapping("/success")
     ResponseEntity<String> success(@ModelAttribute("ics") String ics) {
+        ContentDisposition contentDisposition =
+                ContentDisposition.attachment().filename("calendar.ics", StandardCharsets.UTF_8).build();
+
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + "calendar.ics" +"\"")
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition.toString())
                 .header(HttpHeaders.CONTENT_TYPE, "text/calendar")
                 .body(ics);
     }
